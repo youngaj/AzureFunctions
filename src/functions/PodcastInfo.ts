1ef43bacd7e0f061dff1b8269296249969d98008
Andre Young <dreyoung79@yahoo.com>
2023-12-24 23:06:17 -0500

    refactor to include types and clean up code

--- src/functions/PodcastInfo.ts
+++ src/functions/PodcastInfo.ts
@@ -1,68 +1,28 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import axios, { AxiosStatic } from "axios";
-import { LoginPayload, LoginCredentials } from "./types";
+import {
+  LoginPayload,
+  LoginCredentials,
+  PodCast,
+  StarredEpisode,
+  Episode,
+  PodcastListeningStats,
+  PocketCastStarredEpisodeResponse,
+  PocketCastQueuedEpisodeResponse,
+} from "./types";
 
 export async function PodcastInfo(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   axios.defaults.headers.common["Authorization"] = process.env["pocketCastsAuthorizationToken"];
   await verifyCredentials(axios);
 
-  const podcastResponse = await axios.post("https://api.pocketcasts.com/user/podcast/list");
+  const podcasts = await getPodcasts();
 
-  const queueResponse = await axios.post("https://api.pocketcasts.com/up_next/list", {
-    version: 2,
-    model: "webplayer",
-    serverModified: 0,
-  });
+  const starred = await getStarredPodcasts(podcasts);
 
-  const starredResponse = await axios.post("https://api.pocketcasts.com/user/starred", {});
+  const queue = await getQueuedEpisodes(podcasts);
 
-  let stats = null;
+  let stats = await getPodcastStats();
 
-  //-- function to add show notes
-  const addShowNotes = async (episode) => {
-    const url = `https://podcast-api.pocketcasts.com/episode/show_notes/${episode.uuid}`;
-    try {
-      const response = await axios.get(url);
-      episode.showNotes = response.data.show_notes;
-    } catch (error) {
-      console.error(error.response.statusText, episode);
-    }
-    return episode;
-  };
-
-  const _getPodcast = (podcastUuid, podcasts) => {
-    const podcast = podcasts.find((x) => x.uuid === podcastUuid);
-    return podcast;
-  };
-
-  const podcasts = podcastResponse.data.podcasts;
-  //-- Add show notes to episodes
-  const queue = await Promise.all(
-    queueResponse.data.episodes.map(async (episode) => {
-      try {
-        await addShowNotes(episode);
-      } catch (error) {}
-      episode.podcast = _getPodcast(episode.podcastUuid, podcasts);
-      return episode;
-    })
-  );
-
-  const starred = await Promise.all(
-    starredResponse.data.episodes.map(async (episode) => {
-      try {
-        await addShowNotes(episode);
-      } catch (error) {}
-      episode.podcast = _getPodcast(episode.podcast, podcasts);
-      return episode;
-    })
-  );
-
-  try {
-    const statsResponse = await axios.post("https://api.pocketcasts.com/user/stats/summary");
-    stats = statsResponse.data;
-  } catch (error) {}
-
-  context.log("Axios response is", queueResponse.data);
   const response = {
     queue: queue,
     starred: starred,
@@ -71,6 +31,89 @@ export async function PodcastInfo(request: HttpRequest, context: InvocationConte
   };
 
   return { jsonBody: response };
+}
+
+async function getPodcasts(): Promise<PodCast[]> {
+  const podcastResponse = await axios.post("https://api.pocketcasts.com/user/podcast/list");
+  const podcasts = podcastResponse.data.podcasts;
+  return podcasts;
+}
+
+async function getPodcastStats(): Promise<PodcastListeningStats | null> {
+  let stats: PodcastListeningStats | null = null;
+  try {
+    const statsResponse = await axios.post("https://api.pocketcasts.com/user/stats/summary");
+    stats = statsResponse.data;
+  } catch (error) {
+    console.log(`Error retrieving podcast listening stats`, error);
+  }
+  return stats;
+}
+
+async function getQueuedEpisodes(podcasts: PodCast[]) {
+  const queueResponse = await axios.post<PocketCastQueuedEpisodeResponse>("https://api.pocketcasts.com/up_next/list", {
+    version: 2,
+    model: "webplayer",
+    serverModified: 0,
+  });
+
+  const episodes = queueResponse.data.episodes.map(async (pocketCastQueuedEpisode) => {
+    const episode: Episode = {
+      ...pocketCastQueuedEpisode,
+      podcast: undefined,
+      podcastUuid: pocketCastQueuedEpisode.podcast,
+      showNotes: "",
+    };
+    try {
+      episode.showNotes = await getShowNotes(episode.podcastUuid);
+    } catch (error) {
+      console.error(`Error trying to retrieve showNotes for queued episode ${pocketCastQueuedEpisode.uuid}`, error);
+    }
+    episode.podcast = _getPodcast(episode.podcastUuid, podcasts);
+    return pocketCastQueuedEpisode;
+  });
+
+  //-- Add show notes to episodes
+  const queue = await Promise.all(episodes);
+  return queue;
+}
+
+async function getStarredPodcasts(podcasts: PodCast[]): Promise<StarredEpisode[]> {
+  const starredResponse = await axios.post<PocketCastStarredEpisodeResponse>("https://api.pocketcasts.com/user/starred", {});
+  const episodes = starredResponse.data.episodes.map(async (pocketCastStarredEpisode) => {
+    const starredEpisode: StarredEpisode = {
+      ...pocketCastStarredEpisode,
+      showNotes: "",
+    };
+    try {
+      starredEpisode.showNotes = await getShowNotes(pocketCastStarredEpisode.uuid);
+    } catch (error) {
+      console.error(`Error trying to retrieve showNotes for starred episode ${pocketCastStarredEpisode.uuid}`, error);
+    }
+    starredEpisode.podcast = _getPodcast(pocketCastStarredEpisode.podcastUuid, podcasts);
+    return starredEpisode;
+  });
+
+  const starred = await Promise.all(episodes);
+  return starred;
+}
+
+function _getPodcast(podcastUuid: string, podcasts: PodCast[]) {
+  const podcast = podcasts.find((x) => x.uuid === podcastUuid);
+  return podcast;
+}
+
+//-- function to add show notes
+async function getShowNotes(episodeUUID: string) {
+  let showNotes: string = "";
+  const url = `https://podcast-api.pocketcasts.com/episode/show_notes/${episodeUUID}`;
+  try {
+    const response = await axios.get(url);
+    showNotes = response.data.show_notes;
+  } catch (error) {
+    console.error(error.response.statusText, episodeUUID);
+  }
+  return showNotes;
 }
 
 async function getCredentials() {
@@ -95,7 +138,7 @@ async function verifyCredentials(axios: AxiosStatic) {
 }
 
 app.http("PodcastInfo", {
-  methods: ["GET", "POST"],
+  methods: ["GET"],
   authLevel: "anonymous",
   handler: PodcastInfo,
 });
--- src/functions/types.ts
+++ src/functions/types.ts
@@ -3,6 +3,7 @@ export interface LoginPayload {
   password: string;
   scope: string;
 }
+
 export interface LoginCredentials {
   email: string;
   uuid: string;
@@ -11,4 +12,77 @@ export interface LoginCredentials {
   tokenType: string;
   expiresIn: number;
   refreshToken: string;
+}
+
+export interface PodCast {
+  uuid: string;
+  title: string;
+  url: string;
+  author: string;
+  description: string;
+  lastEpisodePublished: Date;
+}
+
+export interface PodcastListeningStats {
+  timeIntroSkipping: number;
+  timeListened: number;
+  timeSilenceRemoval: number;
+  timeSkipping: number;
+  timeVariableSpeed: number;
+  timesStartedAt: Date;
+}
+
+export interface Episode {
+  uuid: string;
+  podcastUuid: string;
+  podcast?: PodCast;
+  showNotes: string;
+  title: string;
+  url: string;
+}
+
+export interface StarredEpisode extends Episode {
+  published: Date;
+  size: string;
+  episodeNumber: number;
+  duration: number;
+  podcastTitle: string;
+}
+
+export interface PocketCastQueuedEpisodeResponse {
+  episodes: PocketCastQueuedEpisode[];
+}
+
+interface PocketCastQueuedEpisode {
+  title: string;
+  url: string;
+  podcast: string;
+  uuid: string;
+  published: Date;
+}
+
+export interface PocketCastStarredEpisodeResponse {
+  total: number;
+  episodes: PocketCastStarredEpisode[];
+}
+
+interface PocketCastStarredEpisode {
+  uuid: string;
+  url: string;
+  published: Date;
+  duration: number;
+  fileType: string;
+  title: string;
+  size: string;
+  playingStatus: number;
+  playedUpTo: number;
+  starred: boolean;
+  podcastUuid: string;
+  podcastTitle: string;
+  episodeType: string;
+  episodeSeason: number;
+  episodeNumber: number;
+  isDeleted: boolean;
+  author: string;
+  bookmarks: any[];
 }
